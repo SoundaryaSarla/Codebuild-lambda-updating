bd9686bb384be8b3a94f3728def0ce4ccf5584b2
Sarala, Soundarya <soundarya.sarala@ge.com>
2024-05-06 17:48:50 +0530

    added

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('I am testing the lambda code to update')
+        'body': json.dumps('I am testing the lambda code to updated')
     }
